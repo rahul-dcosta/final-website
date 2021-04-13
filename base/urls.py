6494ade62a53af1b201a4be4837a38832b7ba786
Rahul D'Costa <rdcosta@umich.edu>
2021-04-13 22:47:37 +0400

    all links working except logo to home

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('about/', views.about, name='about'),
+    path('blog/', views.blog, name='blog'),
+    path('contact/', views.contact, name='contact'),
+    path('experience/', views.experience, name='experience'),
+    path('projskills/', views.projskills, name='projskills'),
+    path('other/', views.other, name='other')
 ]
--- base/views.py
+++ base/views.py
@@ -5,3 +5,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base/home.html')
+
+def about(request):
+    return render(request, 'base/about.html')
+
+def blog(request):
+    return render(request, 'base/blog.html')
+
+def contact(request):
+    return render(request, 'base/contact.html')
+
+def experience(request):
+    return render(request, 'base/experience.html')
+
+def other(request):
+    return render(request, 'base/other.html')
+
+def projskills(request):
+    return render(request, 'base/projskills.html')
